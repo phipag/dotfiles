c1b881932e4e46c6cc052b371fb366c3408d817f
Philipp Page <github@philipp-page.de>
2021-11-20 13:45:15 +0100

    Add comments to custom.zsh and remove default description

--- zsh/custom/custom.zsh
+++ zsh/custom/custom.zsh
@@ -1,12 +1,6 @@
-# You can put files here to add functionality separated per file, which
-# will be ignored by git.
-# Files on the custom/ directory will be automatically loaded by the init
-# script, in alphabetical order.
-
-# For example: add yourself some shortcuts to projects you often work on.
-#
-# brainstormr=~/Projects/development/planetargon/brainstormr
-# cd $brainstormr
-
+# Init jenv for Java environment management
 eval "$(jenv init -)"
+
+# Init pyenv for Python environment management
 eval "$(pyenv init -)"
+
